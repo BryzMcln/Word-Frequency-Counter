66686f9e47843e408df2c5551a6d53849b8f921e
BryzMcln <jhnbrypntjmcln@gmail.com>
2024-03-08 13:17:13 +0800

    folder set 2

---
+++ Practice_Set_02/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Word Frequency Counter</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+
+</head>
+<body>
+    <h1>Word Frequency Counter</h1>
+    
+    <form action="process.php" method="post">
+        <label for="text">Paste your text here:</label><br>
+        <textarea id="text" name="text" rows="10" cols="50" required></textarea><br><br>
+        
+        <label for="sort">Sort by frequency:</label>
+        <select id="sort" name="sort">
+            <option value="asc">Ascending</option>
+            <option value="desc">Descending</option>
+        </select><br><br>
+        
+        <label for="limit">Number of words to display:</label>
+        <input type="number" id="limit" name="limit" value="10" min="1"><br><br>
+        
+        <input type="submit" value="Calculate Word Frequency">
+    </form>
+</body>
+</html>
